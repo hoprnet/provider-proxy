e820d97a736fa8a6795ecc408998bdb06e6bc3af
Tino Breddin <t@tolbrino.com>
2021-09-08 08:40:35 +0200

    add more request logging

--- src/handler.ts
+++ src/handler.ts
@@ -12,17 +12,27 @@ export async function handleRequest(request: Request): Promise<Response> {
 
   // handle only known providers
   const provider = Object.entries(providers).find(([k]) => k === path);
-  if (provider !== undefined) {
-    const [, providerUrl] = provider;
-    const start = Date.now();
-    return await fetch(providerUrl, request).then(function(response) {
-      const elapsed = Date.now() - start;
-      console.log(`Forwarded request to ${providerUrl} took ${elapsed} ms`);
-      return response;
-    });
+  if (provider === undefined) {
+    // if the provider is not known, return not found
+    console.log(`Unknown request: ${request.url}`)
+    return new Response("not found", { status: 404 });
   }
 
-  // if the provider is not known, return not found
-  console.log(`Unknown request: ${request.url}`)
-  return new Response("not found", { status: 404 });
+  // const method = request.body;
+  const requestJson = await request.clone().json();
+  const method = requestJson.method;
+  const [, providerUrl] = provider;
+  const start = Date.now();
+
+  return fetch(providerUrl, request).then(function(response) {
+    const elapsed = Date.now() - start;
+    const responseClone = response.clone();
+    const responseCloneJson = responseClone.json();
+    let result = "ok";
+    if ('error' in responseCloneJson) {
+      result = `error ${responseCloneJson.error.message}`;
+    }
+    console.log(`Forwarded request to ${path}, processed in ${elapsed} ms, called method ${method}, returned ${result}`);
+    return response;
+  });
 }
