0b40d3d234afa0c8828e2d85d8f546b601a3c36e
Tino Breddin <tino@hoprnet.org>
2022-02-07 14:52:10 +0100

    Update xdai mainnet provider

--- src/providers.ts
+++ src/providers.ts
@@ -4,8 +4,10 @@ export const providers = {
   matic_rio:
     "https://nd-435-704-707.p2pify.com/9ad58676aefc2aea283ca0a66724bea6",
   xdai_mainnet:
-    "https://:39b78eea48bd48c2b687e9e7342bec1e@poa-xdai-archival.gateway.pokt.network/v1/lb/61dbeff75a6d11003821698f",
+    `https://apis.ankr.com/${XDAI_MAINNET_ANKR_API_PROJECT_ID}/${XDAI_MAINNET_ANKR_API_TOKEN}/xdai/fast/main`,
   xdai_mainnet_old:
+    "https://:39b78eea48bd48c2b687e9e7342bec1e@poa-xdai-archival.gateway.pokt.network/v1/lb/61dbeff75a6d11003821698f",
+  xdai_mainnet_old_retired:
     "https://:842d068fca3c53e26c85c5f4eb49e1fc@poa-xdai.gateway.pokt.network/v1/lb/61113fe218656e00369bdb9b",
   eth_gnosis_mainnet: "https://eth-erigon.lsotech.net/",
   eth_gnosis_rinkeby:
