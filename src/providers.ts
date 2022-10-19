84aba33447c8518d73865d6cd54427caeed93bb3
Tino Breddin <tino@hoprnet.org>
2022-10-19 13:14:15 +0200

    Use Ankr Goerli endpoint until ours are back up

--- src/providers.ts
+++ src/providers.ts
@@ -18,8 +18,9 @@ export const providers = {
     "https://mainnet.infura.io/v3/7f8df481afe542f99fe5e01de8822202",
   eth_gnosis_mainnet_alt: "https://eth-erigon.lsotech.net/",
   eth_goerli: [
-    `https://primary.goerli.rpc.hoprtech.net`,
-    `https://secondary.goerli.rpc.hoprtech.net`,
+    `https://rpc.ankr.com/eth_goerli`,
+//    `https://primary.goerli.rpc.hoprtech.net`,
+//    `https://secondary.goerli.rpc.hoprtech.net`,
   ],
   eth_ceramic_mainnet: "https://eth-erigon.lsotech.net/",
   eth_ceramic_testnet:
