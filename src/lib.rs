c8acdf2c4f7d1102a45e0c3691db4e6d6cc3bc4f
Tino Breddin <t@tolbrino.com>
2024-01-08 11:12:51 +0100

    Revert "Add semi-random endpoint selection"
    This reverts commit 00c7932ca4b920732cf8dd19f93474ee68e4ea13.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ mod providers;
 
 use crate::providers::get_provider;
 use hyper;
-use std::str::FromStr;
 use std::str::Utf8Error;
 
 pub use console_error_panic_hook::set_once as set_panic_hook;
@@ -52,16 +51,9 @@ pub async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Resp
         return Response::error("not found", 404);
     }
 
-    // Use request content-length to choose an endpoint semi-randomly.
-    // Thus, requests with the same content length will go to the same endpoint.
     let endpoints = provider.unwrap().endpoints;
-    let content_length = req
-        .headers()
-        .get("content-length")?
-        .map_or(0, |l| usize::from_str(l.as_str()).unwrap_or(0));
-    let endpoint_pos: usize = content_length % endpoints.len();
-    let endpoint = endpoints.get(endpoint_pos).unwrap_or(endpoints.first().unwrap());
-
+    // TODO: choose random endpoint
+    let endpoint = endpoints.first().unwrap();
     // TODO: add elapsed time check
 
     let url = Url::parse(endpoint.url.as_str())?;
