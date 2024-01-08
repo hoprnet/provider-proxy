a2d24c69e58b5cc1c601a4012c34e1f876572190
Tino Breddin <t@tolbrino.com>
2024-01-08 11:12:49 +0100

    Revert "try to add batch support"
    This reverts commit 1141420931ed4d418cf5272101b4a8b0a7eae854.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 mod providers;
 
 use crate::providers::get_provider;
-use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 use std::str::Utf8Error;
@@ -11,26 +10,11 @@ pub use console_error_panic_hook::set_once as set_panic_hook;
 use worker::*;
 
 #[derive(Serialize, Deserialize, Debug)]
-enum RequestBody {
-    EthRequest(EthRequest),
-    EthRequestBatch(Vec<EthRequest>),
-}
-
-#[derive(Serialize, Deserialize, Debug)]
 struct EthRequest {
     method: String,
     jsonrpc: String,
     params: Vec<String>,
     id: i32,
-}
-
-impl RequestBody {
-    fn get_method(&self) -> String {
-        match self {
-            RequestBody::EthRequest(r) => r.method.clone(),
-            RequestBody::EthRequestBatch(_) => "batch".to_string(),
-        }
-    }
 }
 
 fn map_utf8_error(error: Utf8Error) -> worker::Error {
@@ -51,19 +35,15 @@ async fn make_request(
     provider_name: &str,
     method: String,
 ) -> Result<Response> {
-    let start = Utc::now();
     let hyper_response = sender.send_request(request).await.map_err(map_hyper_error)?;
-    let end = Utc::now();
-    let elapsed = end.timestamp_millis() - start.timestamp_millis();
     let buf = hyper::body::to_bytes(hyper_response).await.map_err(map_hyper_error)?;
     let text = std::str::from_utf8(&buf).map_err(map_utf8_error)?;
     let mut response = Response::ok(text)?;
     response.headers_mut().append("Content-Type", "application/json")?;
     let ret_code = response.status_code();
     console_log!(
-        "Forwarded request to {}, processed in {} ms, called method {}, returned http code {}",
+        "Forwarded request to {}, processed in ? ms, called method {}, returned http code {}",
         provider_name,
-        elapsed,
         method,
         ret_code
     );
@@ -99,6 +79,8 @@ pub async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Resp
     let endpoint_pos: usize = content_length % endpoints.len();
     let endpoint = endpoints.get(endpoint_pos).unwrap_or(endpoints.first().unwrap());
 
+    // TODO: add elapsed time check
+
     let url = Url::parse(endpoint.url.as_str())?;
     let socket = Socket::builder()
         .secure_transport(SecureTransport::On)
@@ -106,14 +88,15 @@ pub async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Resp
 
     let (sender, connection) = hyper::client::conn::handshake(socket).await.map_err(map_hyper_error)?;
 
-    let (method, data) = match req.clone_mut()?.json::<RequestBody>().await {
-        Ok(v) => (v.get_method(), serde_json::to_string(&v)),
-        Err(e) => {
-            console_error!("Bad request body: {:?}", e);
-            return Response::error("Bad request", 400);
-        }
-    };
+    let eth_request = req.clone_mut()?.json().await;
 
+    if eth_request.is_err() {
+        let e = eth_request.unwrap_err();
+        console_error!("Bad request body: {:?}", e);
+        return Response::error("Bad request", 400);
+    }
+
+    let data: EthRequest = eth_request.unwrap();
     let mut request = hyper::Request::builder()
         .uri(endpoint.url.clone())
         .method("POST")
@@ -124,7 +107,7 @@ pub async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Resp
     }
 
     let final_request = request
-        .body(hyper::Body::from(data.unwrap()))
+        .body(hyper::Body::from(serde_json::to_string(&data).unwrap()))
         .map_err(map_hyper_http_error)?;
 
     tokio::select!(
@@ -132,6 +115,6 @@ pub async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Resp
             console_error!("Connection exited: {:?}", res);
             Err(worker::Error::RustError("Connection exited".to_string()))
         },
-        result = make_request(sender, final_request, provider_name, method) => result
+        result = make_request(sender, final_request, provider_name, data.method) => result
     )
 }
