f49762671e07abdb16681df5a053f3f87da70153
ausias-armesto <ausiasarmesto@gmail.com>
2024-01-08 13:18:11 +0100

    Use rpch rpc provider

--- src/providers.ts
+++ src/providers.ts
@@ -1,7 +1,8 @@
 export const providers = {
   xdai_mainnet: [
-    `https://primary.gnosis-chain.rpc.hoprtech.net`,
-    `https://secondary.gnosis-chain.rpc.hoprtech.net`
+    `https://gnosis-provider.rpch.tech`
+    // `https://primary.gnosis-chain.rpc.hoprtech.net`,
+    // `https://secondary.gnosis-chain.rpc.hoprtech.net`
   ],
   eth_gnosis_mainnet_infura:
     "https://mainnet.infura.io/v3/7f8df481afe542f99fe5e01de8822202",
