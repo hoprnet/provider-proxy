508f80bd95c66a4bebb9254d367db4a323261bcb
Tino Breddin <t@tolbrino.com>
2024-01-08 11:12:50 +0100

    Revert "Add request log"
    This reverts commit 03cfb70bfb0d112e6ab417c8a38879deb639f7b7.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,13 @@
 mod providers;
 
 use crate::providers::get_provider;
-use serde::{Deserialize, Serialize};
+use hyper;
 use std::str::FromStr;
 use std::str::Utf8Error;
 
 pub use console_error_panic_hook::set_once as set_panic_hook;
 
 use worker::*;
-
-#[derive(Serialize, Deserialize, Debug)]
-struct EthRequest {
-    method: String,
-    jsonrpc: String,
-    params: Vec<String>,
-    id: i32,
-}
 
 fn map_utf8_error(error: Utf8Error) -> worker::Error {
     worker::Error::RustError(format!("Utf8Error: {:?}", error))
@@ -32,21 +24,12 @@ fn map_hyper_http_error(error: hyper::http::Error) -> worker::Error {
 async fn make_request(
     mut sender: hyper::client::conn::SendRequest<hyper::Body>,
     request: hyper::Request<hyper::Body>,
-    provider_name: &str,
-    method: String,
 ) -> Result<Response> {
     let hyper_response = sender.send_request(request).await.map_err(map_hyper_error)?;
     let buf = hyper::body::to_bytes(hyper_response).await.map_err(map_hyper_error)?;
     let text = std::str::from_utf8(&buf).map_err(map_utf8_error)?;
     let mut response = Response::ok(text)?;
     response.headers_mut().append("Content-Type", "application/json")?;
-    let ret_code = response.status_code();
-    console_log!(
-        "Forwarded request to {}, processed in ? ms, called method {}, returned http code {}",
-        provider_name,
-        method,
-        ret_code
-    );
     Ok(response)
 }
 
@@ -62,8 +45,8 @@ pub async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Resp
     let path = req.path();
 
     // handle only known providers
-    let provider_name = path.splitn(2, '/').last().unwrap();
-    let provider = get_provider(provider_name);
+    let provider_name = path.splitn(2, "/").last();
+    let provider = get_provider(provider_name.unwrap());
     if provider.is_none() {
         console_log!("Unsupported request: {}", path);
         return Response::error("not found", 404);
@@ -88,15 +71,7 @@ pub async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Resp
 
     let (sender, connection) = hyper::client::conn::handshake(socket).await.map_err(map_hyper_error)?;
 
-    let eth_request = req.clone_mut()?.json().await;
-
-    if eth_request.is_err() {
-        let e = eth_request.unwrap_err();
-        console_error!("Bad request body: {:?}", e);
-        return Response::error("Bad request", 400);
-    }
-
-    let data: EthRequest = eth_request.unwrap();
+    let mut myreq = req.clone_mut()?;
     let mut request = hyper::Request::builder()
         .uri(endpoint.url.clone())
         .method("POST")
@@ -107,7 +82,7 @@ pub async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Resp
     }
 
     let final_request = request
-        .body(hyper::Body::from(serde_json::to_string(&data).unwrap()))
+        .body(hyper::Body::from(myreq.bytes().await?))
         .map_err(map_hyper_http_error)?;
 
     tokio::select!(
@@ -115,6 +90,6 @@ pub async fn main(req: Request, _env: Env, _ctx: worker::Context) -> Result<Resp
             console_error!("Connection exited: {:?}", res);
             Err(worker::Error::RustError("Connection exited".to_string()))
         },
-        result = make_request(sender, final_request, provider_name, data.method) => result
+        result = make_request(sender, final_request) => result
     )
 }
